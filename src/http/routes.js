47e27536ef065b207aad8ba4f8ee73d5cb9cd7ac
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2024-02-14 17:55:19 -0300

    uma hora vai

--- src/http/routes.js
+++
@@ -1,8 +0,0 @@
-import Router from 'express';
-import chat from '../gemini/chat.js';
-
-const routes = Router();
-
-routes.post('/', chat);
-
-export default routes;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import routes from './http/routes.js'
+// app.js
 
-const port = process.env.PORT || 9999;
+import express from 'express';
+import bodyParser from 'body-parser';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import dotenv from 'dotenv';
 
 const app = express();
+
+app.use(bodyParser.json());
 dotenv.config();
 
-app.use(express.json());
-app.use(routes)
+const port = process.env.PORT || 9999
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
+let chatHistory = [];
+
+app.get('/chat', (req, res) => {
+  res.json(chatHistory);
+});
+
+app.post('/chat', async (req, res) => {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const userMessage = req.body.message;
+  const result = await model.generateContentStream(userMessage, {
+    context: {
+      chatHistory
+    }
+  });
+
+  chatHistory.push({ type: 'user', message: userMessage });
+  const aiMessage = await result.response;
+  chatHistory.push({ type: 'ai', message: aiMessage });
+
+  res.json({ success: true, message: 'Mensagem enviada com sucesso!' });
+});
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-})
+  console.log(`Server listening at http://localhost:${port}`);
+});
