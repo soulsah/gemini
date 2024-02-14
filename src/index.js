e31ea721b18d6030a664d17a8984872ca513ac41
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2024-02-14 15:34:18 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  const prompt = "Write a story about a magic backpack."
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
