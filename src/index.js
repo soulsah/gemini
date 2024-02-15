80deec4d3e6ba10c2fe1042886c79eda2d241aea
Matheus dos Santos Souza <andzedd.matheus@gmail.com>
2024-02-15 13:23:09 -0300

    minor changes

--- src/index.js
+++ src/index.js
@@ -15,8 +15,56 @@ dotenv.config();
 const port = process.env.PORT || 9999;
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-let history = [];
-
+let history = [
+  {
+    role: "user",
+    parts: "Saudação inicial: Cumprimente o cliente e agradeça por entrar em contato com o serviço de consignado do grupo AMP.",
+  },
+  {
+    role: "model",
+    parts: "Coleta de informações: Solicite ao cliente seu nome para registro, destacando que as informações serão utilizadas apenas para o processo de consignação e serão tratadas com total confidencialidade.",
+  },
+  {
+    role: "user",
+    parts: "Informações sobre o consignado: Explique brevemente o que é o serviço de consignado oferecido pelo grupo AMP, ressaltando suas vantagens, taxas competitivas e condições favoráveis.",
+  },
+  {
+    role: "model",
+    parts: "Valor do empréstimo: Pergunte ao cliente qual o valor aproximado que ele está buscando e em quantas parcelas para o empréstimo consignado.",
+  },
+  {
+    role: "user",
+    parts: "Taxa de juros: Informe ao cliente sobre a taxa de juros do consignado do grupo AMP e destaque os benefícios de escolher essa opção.",
+  },
+  {
+    role: "model",
+    parts: "Documentação necessária: Explique de forma clara e objetiva os documentos necessários para dar continuidade ao processo de consignação.",
+  },
+  {
+    role: "user",
+    parts: "Aprovação de crédito: Explique ao cliente o processo de análise e aprovação de crédito, destacando a agilidade e transparência do grupo AMP nesse processo.",
+  },
+  {
+    role: "model",
+    parts: "Dúvidas e esclarecimentos: Dê a oportunidade para o cliente esclarecer dúvidas e faça um resumo das informações fornecidas até o momento.",
+  },
+  {
+    role: "user",
+    parts: "Formalização do contrato: Caso o cliente esteja interessado, forneça orientações sobre a formalização do contrato, agendamento de assinatura e demais procedimentos necessários.",
+  },
+  {
+    role: "model",
+    parts: "Agradecimento e despedida: Agradeça ao cliente pela escolha do grupo AMP para o serviço de consignado, reforçando o compromisso com a qualidade no atendimento.",
+  },
+  {
+    role: "user",
+    parts: "Vamos iniciar o atendimento"
+  },
+  {
+    role: "model",
+    parts: "Olá! Agradecemos por entrar em contato com o serviço de consignado do grupo AMP. Podemos te ajudar com isso. Para começar, informe seu nome para darmos início ao atendimento"
+  }
+];
 app.get('/chat', (req, res) => {
   res.json(history);
 });
